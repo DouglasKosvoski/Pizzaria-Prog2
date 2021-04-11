8adcabdf65231199242762f9782f7ad77d381780
Douglas Kosvoski <douglas.contactpro@gmail.com>
2021-04-11 14:02:55 -0300

    formulario e validacao da pagina de pedidos

--- src/js/app.js
+++ src/js/app.js
@@ -27,6 +27,60 @@ function show_menu() {
   }
 }
 
+function sabores() {
+  var tamanho = document.getElementById("tamanho");
+  var qtd_sabores = document.getElementById("qtd_sabores");
+  var sabores = document.querySelectorAll('input[type="checkbox"]:checked');
+  var bordas = document.querySelector('input[name="radio"]:checked').value;
+  var max_quantidade = 0;
+  
+  console.log(bordas);
+  
+  switch (tamanho.value) {
+    case "pequena":
+      max_quantidade = 2;
+      break;
+    case "media":
+      max_quantidade = 3;
+      break;
+    case "grande":
+      max_quantidade = 4;
+      break;
+    case "familia":
+      max_quantidade = 5;
+      break;
+    default:
+      max_quantidade = 0;
+      break;
+  }
+  qtd_sabores.innerHTML = "Você selecionou " + sabores.length + " de " + max_quantidade + " sabores.";
+
+  if(sabores.length > max_quantidade && max_quantidade != 0) {
+    alert("Você selecionou mais sabores do que essa Pizza suporta!");
+  }
+}
+
+function finalizar_pedido() {
+  var tamanho = document.getElementById("tamanho").value;
+  var qtd_sabores = document.getElementById("qtd_sabores").innerHTML;
+  var sabores = document.querySelectorAll('input[type="checkbox"]:checked').value;
+  var bordas = document.querySelector('input[name="radio"]:checked').value;
+  
+  if (tamanho == "Vazio") {
+    alert("Selecione o tamanho da Pizza");
+  }
+  else if (sabores == 0) {
+    alert("Selecione o sabor da Pizza");
+  }
+  else {
+    alert(
+      "Tamanho: " + tamanho +
+      "\n" + qtd_sabores +
+      "\nBorda: " + bordas
+    );
+  }
+}
+
 window.onresize = function() {
   largura = window.innerWidth;
   var nav = document.getElementById("navbar");
